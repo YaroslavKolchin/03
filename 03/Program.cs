b43bc25121b54381908d9a7cde557ee7b2335a61
YaroslavKolchin <39592718+YaroslavKolchin@users.noreply.github.com>
2019-03-07 11:08:47 +0300

    Add files via upload

---
+++ 03/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForYaroslav
+{
+    class Program
+    {
+        public static void ShowIntArray(int[] array)   
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i+" ");
+            }
+        }
+
+        public static int ShowMaxInt(int[] array)
+        {
+            return array.Max();
+        }
+
+        public static int ShowMaxEven(int[] array)
+        {
+            Array.Sort(array);
+            int a = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0 && a<array[i])
+                {
+                    a = array[i];
+                }
+            }
+            return a;
+        }
+
+        public static void DelByIndex(ref int[] data, int delIndex)
+        {
+            int[] newData = new int[data.Length - 1];
+            for (int i = 0; i < delIndex; i++)
+            {
+                newData[i] = data[i];
+            }
+            for (int i = delIndex; i < newData.Length; i++)
+            {
+                newData[i] = data[i + 1];
+            }
+            data = newData;
+        }
+
+        public static void DelByValue(ref int[] data, int delValue)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == delValue)
+                {
+                    DelByIndex(ref data, i);
+                    i--;
+                }
+            }
+        }
+
+        public static void NullEvenRows(ref int[,] data)
+        {
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                if (data[i,i]%2==0)     
+                {
+                    for (int j = 0; j < data.GetLength(1); j++)
+                    {
+                        data[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+        public static int ReadUntilPoint()
+        {
+            bool pointBool = true;
+            string s = "";
+            char a;
+            int SpaceNum = 0;
+            while (pointBool)
+            {
+                a = Console.ReadKey().KeyChar;
+                s += a;
+                if (a == '.')
+                {
+                    pointBool = false;
+                }
+            }
+
+            foreach (var i in s)
+            {
+                if (i == ' ')
+                {
+                    SpaceNum++;
+                }
+            }
+
+            return SpaceNum;
+        }
+
+        static void Main(string[] args)
+        {
+            //int[] array = new int[] { 1, 8, 3, 6, 5, 0, 2, 6, 4, 9 };
+            // ShowMaxInt(array);
+            int a = ReadUntilPoint();
+            Console.WriteLine();
+            Console.WriteLine(a+" пробелов");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 03/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleAppForYaroslav")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppForYaroslav")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0ae3b4f6-b43d-48d1-acc3-a43b0b0dc0c3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
